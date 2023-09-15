a052f359abc1e1078aa917b8b287d228e1d76e51
ibrahim-anas <78625924+ibrahim-anas@users.noreply.github.com>
2023-09-15 10:33:37 -0400

    add make-multi-filter.js

---
+++ make-multi-filter.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function MakeMultiFilter(originalArray) {
+    let currentArray = originalArray
+    return arrayFilterer()
+}
+
+function arrayFilterer(filterCriteria, callback) {
+    if (typeof filterCriteria !== 'function') {
+        //return currentArray
+    }
+    return this
+}
+
+function filterCriteria(elem) {
+    for (let i = 0; i < elem.length; i++) {
+
+    }
+}
+function callback() {
+
+}
