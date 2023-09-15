004fd480b5632067ea01c1aa564bb26c70d65160
Patrick Flynn <pflynn157@gmail.com>
2023-09-15 09:43:48 -0400

    Get template processor working

--- template-processor.js
+++ template-processor.js
@@ -1,21 +1,49 @@
 'use strict';
 
-
 class TemplateProcessor {
-
-  constructor(template){
-    this.template = template;
-  }
-
-  fillIn(dictionary) {
-
-    //Get process dictionary
-    var result = Object.keys(dictionary).map(k => ({ [k]: dictionary[k]}));
-
-    //For each KV, replace template with KV
-    // foreach...
-    this.template.replace(/\{[^}]*\}\}/g, dictionary);
-
-  }
-
+    constructor(template) {
+        this.template = template;
+    }
+    
+    fillIn(dictionary) {
+        console.log(this.template);
+        var updated = "";
+        for (var i = 0; i<this.template.length; i++) {
+            var c = this.template[i];
+            if (c == '{') {
+                var c2 = this.template[i+1];
+                if (c2 != '{') {
+                    updated += c2;
+                    ++i;
+                    continue;
+                }
+                i++;
+                
+                var keyword = "";
+                for (var j = i+1; j<this.template.length; j++, i++) {
+                    var c1 = this.template[j];
+                    var c2 = this.template[j+1];
+                    if (c1 == '}' && c2 == '}') {
+                        i += 2;
+                        break;
+                    } else {
+                        keyword += c1;
+                    }
+                }
+                
+                if (keyword in dictionary) {
+                    updated += dictionary[keyword];
+                } else {
+                    updated += "";
+                }
+            } else {
+                updated += c;
+            }
+        }
+        console.log(updated);
+        return updated;
+    }
 }
+
+exports.TemplateProcessor = TemplateProcessor;
+
